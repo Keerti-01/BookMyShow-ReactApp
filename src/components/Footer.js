f526bbc09b80d21a1c71abcfce7df4aadc53ff43
Keerti <86554332+Keerti-01@users.noreply.github.com>
2022-02-02 19:22:13 +0530

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,30 +43,126 @@ function Footer() {
                     <p style={{fontSize:"13px"}}>MOVIES PLAYING IN HYDERABAD</p>
                     <p style={{fontSize:"13px"}}>Bangaraju | Rowdy Boys | Hero(2022) | Super Machi | The King's Man | Pushpa: The Rise part-1</p>
                 </div>
+                 <div>
+                    <p style={{fontSize:"13px"}}>UPCOMING MOVIES</p>
+                    <p style={{fontSize:"13px"}}>Thamezharasan | Saamanyudu | Veerame Vaagai Soodum | Zombivli | 
+                    8/12 Binay Badai Dinesh | Gangs of 18 | DJ Tillu | Scream 5 | Iswarya Murugan | 
+                    Tujhya Sathi Kahi Hi | Gormati | DSJ-Deyyamtho Sahajeevanam | Ombatthane Dikku | 
+                    Varchasva | Luck Down Be Positive | Jai Bheem | #MUTE | Swa | Sila Nerangalil Sila Manithargal |
+                    Dorakuna Ituvanti Seva (DIS)</p>
+                    <br></br>
+                </div>
+                
                 <div>
-                    <p style={{fontSize:"13px"}}>MOVIES PLAYING IN HYDERABAD</p>
-                    <p style={{fontSize:"13px"}}>Bangaraju | Rowdy Boys | Hero(2022) | Super Machi | The King's Man | Pushpa: The Rise part-1</p>
+                    <p style={{fontSize:"13px"}}>MOVIES BY GENRE</p>
+                    <p style={{fontSize:"13px"}}>Drama Movies | Action movies | Comedy Movies | Thriller Movies | 
+                    Romantic Movies | Crime Movies | Horror Movies | Adventure Movies | Sci-fi Movies | 
+                    Devotional Movies | Mystery Movies | Fantasy Movies | Historical Movies | Sports Movies |
+                    Animation Movies | Classic Movies | Adult Movies | Political Movies | War Movies | Heist Movies | 
+                    Musical Movies | Mythoogical Movies | Noir Movies | Anime Movies | Psychological Movies</p>
+                    <br></br>
                 </div>
                 <div>
-                    <p style={{fontSize:"13px"}}>MOVIES PLAYING IN HYDERABAD</p>
-                    <p style={{fontSize:"13px"}}>Bangaraju | Rowdy Boys | Hero(2022) | Super Machi | The King's Man | Pushpa: The Rise part-1</p>
+                    <p style={{fontSize:"13px"}}>MOVIES BY LANGUAGE</p>
+                    <p style={{fontSize:"13px"}}>Movies in Telugu | Movies in Hindi | Movies in English | Movies in Nepali |
+                    Movies in Haryanavi | Movies in Tulu | Movies in Sindhi | Movies in Japanese | Movies in Assamese | 
+                    Movies in Portuguese | Movies in Chattisgarhi | Movies in Khadi | Movies in Malayam | Movies in Bhopuri | 
+                    Movies in French | Movies in Punjabi | Movies in Tamil | Movies in Urdu | Movies in English 7D |
+                    Movies in Oriya | Movies in Marathi | Movies in Kannada | Movies in Konkani | Movies in Rajasthan | 
+                    Movies in Gujarati | Movies in Bengali</p>
+                    <br></br>
                 </div>
                 <div>
-                    <p style={{fontSize:"13px"}}>MOVIES PLAYING IN HYDERABAD</p>
-                    <p style={{fontSize:"13px"}}>Bangaraju | Rowdy Boys | Hero(2022) | Super Machi | The King's Man | Pushpa: The Rise part-1</p>
+                    <p style={{fontSize:"13px"}}>PLAYS BY GENRE IN HYDERABAD</p>
+                    <p style={{fontSize:"13px"}}>Comedy Plays | Drama Plays | Broadway Plays | Sci-fi Plays | Period Plays | 
+                    Tragedy Plays | Action Plays | Adaptation Plays | Mystery Plays | Fantasy Plays | Historical Plays | 
+                    Online Streaming Plays | Classic Plays | Adult Plays | Contemporary Plays | Tribute Plays | Political Plays | 
+                    Thriller Plays | Suspense Plays | Romantic Plays | Biography Plays | Horror Plays | War Plays | 
+                    Adventure Plays | Musical Plays | Mythological Plays | Noir Plays | Regional Plays | Psychological Plays </p>
+                    <br></br>
                 </div>
                 <div>
-                    <p style={{fontSize:"13px"}}>MOVIES PLAYING IN HYDERABAD</p>
-                    <p style={{fontSize:"13px"}}>Bangaraju | Rowdy Boys | Hero(2022) | Super Machi | The King's Man | Pushpa: The Rise part-1</p>
+                    <p style={{fontSize:"13px"}}>MOVIE REVIEWS</p>
+                    <p style={{fontSize:"13px"}}>Trending Articles | Latest News on Movies,Events,Plays and Sports | Anupama
+                    Chopra Movie Reviews | Film Reviews | Guest Blogging</p>
+                    <br></br>
                 </div>
                 <div>
-                    <p style={{fontSize:"13px"}}>MOVIES PLAYING IN HYDERABAD</p>
-                    <p style={{fontSize:"13px"}}>Bangaraju | Rowdy Boys | Hero(2022) | Super Machi | The King's Man | Pushpa: The Rise part-1</p>
+                    <p style={{fontSize:"13px"}}>MOVIE UPDATES AND CELEBRITIES</p>
+                    <p style={{fontSize:"13px"}}>Upcoming Movies | Now Showing | Movie Celebrities </p>
+                    <br></br>
                 </div>
                 <div>
-                    <p style={{fontSize:"13px"}}>MOVIES PLAYING IN HYDERABAD</p>
-                    <p style={{fontSize:"13px"}}>Bangaraju | Rowdy Boys | Hero(2022) | Super Machi | The King's Man | Pushpa: The Rise part-1</p>
+                    <p style={{fontSize:"13px"}}>THINGS TO DO IN HYDERABAD</p>
+                    <p style={{fontSize:"13px"}}>Adventure | Amusement Parks | Antiques,Heritage,Museums | Christmas Celebrations | 
+                     Durga Puja Festival | Festivals | Foods and Drinks | Gaming | Hilsa Festival | Holi Parties | 
+                     Hospitals | Monuments | Navratri | Nightlife | New Year PArties | Quizzes and Competitions | Shops and
+                     Malls | Tourists Attractions | Unique Tours | Valentines Day Celebrations</p>
+                     <br></br>
                 </div>
+                <div>
+                    <p style={{fontSize:"13px"}}>SPORTS EVENTS IN HYDERABAD</p>
+                    <p style={{fontSize:"13px"}}>E Sports | Rugby | Archery | Athletics | Badminton | Baseball | Basketball
+                    | Boat Race | Bowling | Boxing | Card Games | Chess | Cricket | Cycling | Derby Horse Race | 
+                    Golf | Football | Ice Skating | Hockey | Judo | Kabaddi | KickBoxing | Online Games | Wrestling | Weight
+                     Lifting | Volleyball | Triathion | Tennis | Table Tennis | Swimming | Snooker | Shooting | 
+                     Sailing | Polo | Online Sports | Muay Thai | Motorsports | Mixed Martial Arts</p>
+                     <br></br>
+                </div>
+                <div>
+                    <p style={{fontSize:"13px"}}>EVENTS IN HYDERABAD</p>
+                    <p style={{fontSize:"13px"}}>Workshops | Comedy Shows | Kids | Music Shows | Spirituality | Award Shows 
+                    | Conferences | Exhibitions | Meetups | Online Streaming </p>
+                    <br></br>
+                </div>
+                <div>
+                    <p style={{fontSize:"13px"}}>CINEMAS IN TOP CITIES</p>
+                    <p style={{fontSize:"13px"}}>Workshops | Comedy Shows | Kids | Music Shows | Spirituality | Award Shows 
+                    | Conferences | Exhibitions | Meetups | Online Streaming </p>
+                    <br></br>
+                </div>
+                <div>
+                    <p style={{fontSize:"13px"}}>EVENTS IN TOP CITIES</p>
+                    <p style={{fontSize:"13px"}}>Workshops | Comedy Shows | Kids | Music Shows | Spirituality | Award Shows 
+                    | Conferences | Exhibitions | Meetups | Online Streaming </p>
+                    <br></br>
+                </div>
+                <div>
+                    <p style={{fontSize:"13px"}}>PLAYS IN TOP CITIES</p>
+                    <p style={{fontSize:"13px"}}>Workshops | Comedy Shows | Kids | Music Shows | Spirituality | Award Shows 
+                    | Conferences | Exhibitions | Meetups | Online Streaming </p>
+                    <br></br>
+                </div>
+                <div>
+                    <p style={{fontSize:"13px"}}>ACTIVITIES IN TOP CITIES</p>
+                    <p style={{fontSize:"13px"}}>Workshops | Comedy Shows | Kids | Music Shows | Spirituality | Award Shows 
+                    | Conferences | Exhibitions | Meetups | Online Streaming </p>
+                    <br></br>
+                </div>
+                <div>
+                    <p style={{fontSize:"13px"}}>MOVIES IN TOP CITIES</p>
+                    <p style={{fontSize:"13px"}}>Workshops | Comedy Shows | Kids | Music Shows | Spirituality | Award Shows 
+                    | Conferences | Exhibitions | Meetups | Online Streaming </p>
+                    <br></br>
+                </div>
+                <div>
+                    <p style={{fontSize:"13px"}}>COUNTRIES</p>
+                    <p style={{fontSize:"13px"}}>Indonesia | Sri Lanka | UAE | Singapore | West Indies </p>
+                    <br></br>
+                </div>
+                <div>
+                    <p style={{fontSize:"13px"}}>HELP</p>
+                    <p style={{fontSize:"13px"}}>About Us | Contact Us | Current Opening | Press Release | Press Coverage | Sitemap
+                    | FAQs | Terms and Conditions | Privacy Policy </p>
+                    <br></br>
+                </div>
+                <div>
+                    <p style={{fontSize:"13px"}}>BOOK MY SHOW EXCLUSIVES</p>
+                    <p style={{fontSize:"13px"}}>Superstar | BookASmile | Corporate Vouchers | Gift Cards | List my Show | Offers 
+                    | Stream | Buzz | Trailers </p>
+                    <br></br>
+                </div>
+                
                 <div>
                     <hr style={{marginTop:"40px",marginBottom: "60px"}}></hr>
                 </div>
